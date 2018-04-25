f35b2e268f1c51b429c2b8af5cd0fd892b43f589
quydx <quy196hp8@gmail.com>
2018-04-25 11:31:23 +0700

    update

--- rest/urls.py
+++ rest/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^api/initalization$', views.backup_init, name='backup-init'),
     url(r'^api/metadata$', views.process_metadata, name='backup-process-metadata'),
+    url(r'^api/data$', views.process_data, name='backup-process-data'),
 ]
