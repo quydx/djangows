7d8538ba005d6a97a005dd44cb6b9d66c485c885
quydx <quy196hp8@gmail.com>
2018-04-26 23:19:27 +0700

    models

--- rest/admin.py
+++ rest/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(FileObj)
+admin.site.register(File)
+admin.site.register(Attr)
+admin.site.register(AttrValue)
--- rest/models.py
+++ rest/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FileObj(models.Model):
+    file_system = models.CharField(max_length=256)
+
+    def __str__(self):
+        return "%s" % self.file_system
+
+
+class File(models.Model):
+    name = models.CharField(max_length=256)
+    file_system = models.ForeignKey('FileObj', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s" % self.name
+
+
+class Attr(models.Model):
+    name = models.CharField(max_length=256)
+    type = models.IntegerField(default=0)
+    obj = models.ForeignKey(
+        'FileObj',
+        on_delete=models.CASCADE,
+    )
+    type = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "%s" % self.name
+
+
+class AttrValue(models.Model):
+    attr = models.ForeignKey(
+        'Attr',
+        on_delete=models.CASCADE
+    )
+    value = models.CharField(max_length=256)
+    file = models.ForeignKey(
+        'File',
+        on_delete=models.CASCADE
+    )
+    value = models.CharField(max_length=256)
+
+    def __str__(self):
+        return "%s" % self.id
+
